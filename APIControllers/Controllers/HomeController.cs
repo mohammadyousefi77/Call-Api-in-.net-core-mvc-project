2cedde4159040f39e57dc73ff0122829da947e3f
mohammad <mrmy98@gmail.com>
2023-02-19 15:16:51 +0330

    call Api in .net core mvc project

---
+++ APIControllers/Controllers/HomeController.cs
@@ -0,0 +1,158 @@
+﻿using APIControllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace APIControllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Reservation> reservationList = new List<Reservation>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7175/api/Reservation"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<Reservation>>(apiResponse);
+                }
+            }
+            return View(reservationList);
+        }
+
+
+        public ViewResult AddReservation() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddReservation(Reservation reservation)
+        {
+            Reservation receivedReservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(reservation), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:7175/api/Reservation", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(receivedReservation);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetReservation(Reservation reservation)
+        {
+            Reservation receivedReservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(reservation), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("https://localhost:7175/api/Reservation", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(receivedReservation);
+        }
+
+
+        // public ViewResult GetReservation() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> GetReservation(int id)
+        {
+            Reservation reservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7175/api/Reservation/" + id))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                    else
+                        ViewBag.StatusCode = response.StatusCode;
+                }
+            }
+            return View(reservation);
+        }
+
+
+        public async Task<IActionResult> UpdateReservation(int id)
+        {
+            Reservation reservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7175/api/Reservation/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(reservation);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateReservation(Reservation reservation)
+        {
+            Reservation receivedReservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                var content = new MultipartFormDataContent();
+                content.Add(new StringContent(reservation.Id.ToString()), "Id");
+                content.Add(new StringContent(reservation.Name), "Name");
+                content.Add(new StringContent(reservation.StartLocation), "StartLocation");
+                content.Add(new StringContent(reservation.EndLocation), "EndLocation");
+
+                using (var response = await httpClient.PutAsync("https://localhost:7175/api/Reservation", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ViewBag.Result = "Success";
+                    receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(receivedReservation);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteReservation(int ReservationId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("https://localhost:7175/api/Reservation/" + ReservationId))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ViewResult AddFile() => View();
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile file)
+        {
+            string apiResponse = "";
+            using (var httpClient = new HttpClient())
+            {
+                var form = new MultipartFormDataContent();
+                using (var fileStream = file.OpenReadStream())
+                {
+                    form.Add(new StreamContent(fileStream), "file", file.FileName);
+                    using (var response = await httpClient.PostAsync("https://localhost:7175/api/Reservation/UploadFile", form))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            return View((object)apiResponse);
+        }
+    }
+}
---
+++ APIControllers/Controllers/ReservationController.cs
@@ -0,0 +1,80 @@
+﻿using APIControllers.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIControllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private IWebHostEnvironment webHostEnvironment;
+        private IRepository repository;
+        public ReservationController(IRepository repo, IWebHostEnvironment environment)
+        {
+            repository = repo;
+            webHostEnvironment = environment;
+        }
+
+
+        //private IRepository repository;
+        //public ReservationController(IRepository repo) => repository = repo;
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => repository.Reservations;
+
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> Get(int id)
+        {
+            if (id == 0)
+                return BadRequest("Value must be passed in the request body.");
+            return Ok(repository[id]);
+        }
+
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) =>
+       repository.AddReservation(new Reservation
+       {
+           Name = res.Name,
+           StartLocation = res.StartLocation,
+           EndLocation = res.EndLocation
+       });
+
+
+        [HttpPut]
+        public Reservation Put([FromForm] Reservation res) => repository.UpdateReservation(res);
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody] JsonPatchDocument<Reservation> patch)
+        {
+            var res = (Reservation)((OkObjectResult)Get(id).Result).Value;
+            if (res != null)
+            {
+                patch.ApplyTo(res);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repository.DeleteReservation(id);
+
+
+        [HttpPost("UploadFile")]
+        public async Task<string> UploadFile([FromForm] IFormFile file)
+        {
+            string path = Path.Combine(webHostEnvironment.WebRootPath, "icon/" + file.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return "https://localhost:7175/icon/" + file.FileName;
+        }
+
+    }
+
+
+}
+
+ 
+        
